01db35507904e0955e01c528d1bd32b6981bbc23
franco <fluffins007@outlook.com>
2025-12-17 16:58:57 -0500

    Switched the backend to libfmake

---
+++ libfmake.py
@@ -0,0 +1,71 @@
+import os
+def readConfig(configName):
+    if not os.path.exists(configName):
+        print("WARN | No such config available for reading")
+        return ("gcc","main.c","main",True,"Compiling done!",False,False,"-j$(nproc)")
+    f = open(configName,"r")
+    cc = "gcc"
+    src = "main.c"
+    out = "main"
+    msg = True
+    say = "Compiling done!"
+    run = False
+    mke = False
+    mkf = "-j$(nproc)"
+
+    for i in f:
+        z = i.strip().split()
+        if len(z) == 0:
+            pass
+        if len(z) >= 3:
+            if z[0] == "cc":
+                cc = z[2]
+            if z[0] == "src":
+                src = z[2]
+            if z[0] == "out":
+                out = z[2]
+            if z[0] == "msg":
+                if z[2] == "true":
+                    msg = True
+                if z[2] == "false":
+                    msg = False
+            if z[0] == "say":
+                x = i.strip()
+                while x[0] != "=":
+                    x = x[1:]
+                x = x[1:]
+                while x[0] == " ":
+                    x = x[1:]
+                say = x
+            if z[0] == "run":
+                if z[2] == "true":
+                    run = True
+                if z[2] == "false":
+                    run = False
+            if z[0] == "mke":
+                if z[2] == "false":
+                    mke = False
+                if z[2] == "true":
+                    mke = True
+            if z[0] == "mkf":
+                x = i.strip()
+                while x[0] != "=":
+                    x = x[1:]
+                x = x[1:]
+                while x[0] == " ":
+                    x = x[1:]
+
+    return (cc,src,out,msg,say,run,mke,mkf)
+
+def genConfig(fileName="make.fmake",overWrite=False,gCC="gcc",gSRC="main.c",gOUT="main",gMSG=True,gSAY="Compiling done!",gRUN=False,gMKE=False,gMKF="-j$(nproc)"):
+    
+    if os.path.exists(fileName) and not overWrite:
+        print("WARN | overWrite is set to True, skipping overwrite")
+        return -1
+
+    elif os.path.exists(fileName) and overWrite or not os.path.exists(fileName):
+        f = open(fileName,"w")
+        f.write(f"cc = {gCC}\nsrc = {gSRC}\nout = {gOUT}\nmsg = {str(gMSG).lower()}\nsay = {gSAY}\nrun = {str(gRUN).lower()}\nmke = {str(gMKE).lower()}\nmkf = {gMKF}")
+        f.close()
+        return 0
+        
